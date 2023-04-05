641a81ddf3b071756233801f57740807ef916681
Artur Matysiak <inz.matys@gmail.com>
2023-04-05 14:27:40 +0200

    - add geolocation from Internet Sercvices Provider - clean code

--- main.js
+++ main.js
@@ -1,20 +1,45 @@
-// API POBIERAJĄCE POGODĘ
-const Weather = () => {
-  const urlWeather =
-    "https://api.open-meteo.com/v1/forecast?latitude=50.15&longitude=18.84&hourly=temperature_2m,cloudcover,windspeed_80m,temperature_180m&models=gfs_global&current_weather=true&forecast_days=3&timezone=Europe%2FBerlin";
-  // Podstawowa! budowa metody fetch()
-  fetch(urlWeather)
-    .then((response) => {
-      if (response.status !== 200) {
+let longitudeGeo;
+let latitudeGeo;
+let cityGeo;
+
+// API POBIERAJĄCE GEOLOKALIZACJĘ wg IP
+const Geo = () => {
+  const urlGeo = "https://get.geojs.io/v1/ip/geo.json";
+  fetch(urlGeo)
+    .then((geo) => {
+      if (geo.status !== 200) {
         throw Error("To nie jest odp. 200");
       } else {
-        return response.json(); // parsuje tzn. zamienia wyniki z url na obiekt JS
+        return geo.json();
       }
     })
-    // .then((json) => console.log(json))
+    .then((geo) => {
+      longitudeGeo = geo.longitude;
+      latitudeGeo = geo.latitude;
+      cityGeo = geo.city;
+      Weather();
+    })
+    .catch((err) => alert(err, "coś z Geolokalizacją jest nie tak!"));
+};
+
+// API POBIERAJĄCE POGODĘ
+const Weather = () => {
+  const urlWeather = `https://api.open-meteo.com/v1/forecast?latitude=${latitudeGeo}&longitude=${longitudeGeo}&hourly=temperature_2m,cloudcover,windspeed_80m,temperature_180m&models=gfs_global&current_weather=true&forecast_days=1&timezone=Europe%2FBerlin`;
+
+  // Podstawowa! budowa metody fetch()
+  fetch(urlWeather)
+    .then((climate) => {
+      if (climate.status !== 200) {
+        throw Error("To nie jest odp. 200");
+      } else {
+        return climate.json(); // parsuje tzn. zamienia wyniki z url na obiekt JS
+      }
+    })
+    .then((climate) => showWeather(climate))
     .catch((err) => alert(err, "coś z Pogodą jest nie tak!"));
 };
 
+// API Z KAWĄ
 const Coffee = () => {
   const urlCoffee = "https://coffee.alexflipnote.dev/random.json";
   fetch(urlCoffee)
@@ -41,12 +66,47 @@ const Cats = () => {
 };
 
 // WYŚWIETL WYNIKI w DOM
-const showWeather = () => {};
+const showWeather = (climate) => {
+  const resultArea = document.querySelector(".weather__grid");
+
+  // add city + date
+  const city = document.createElement("div");
+  const temp = [...climate.hourly.temperature_2m];
+  city.textContent = `${cityGeo} (twoja lokalizacja wg ISP), aktualizacja ${climate.current_weather.time}`;
+  city.classList.add("city__my");
+  resultArea.appendChild(city);
+
+  // add temp.
+  for (let i = 14; i < temp.length; i++) {
+    const text = document.createElement("div");
+    text.textContent = `${temp[i]}°C`;
+    text.classList.add("t");
+    resultArea.appendChild(text);
+  }
+
+  // add wind
+  const wind = [...climate.hourly.windspeed_80m];
+  for (let i = 14; i < wind.length; i++) {
+    const text = document.createElement("div");
+    text.textContent = `${wind[i]} km/h`;
+    text.classList.add("w");
+    resultArea.appendChild(text);
+  }
+
+  // add cloud
+  const cloud = [...climate.hourly.cloudcover];
+  for (let i = 14; i < cloud.length; i++) {
+    const text = document.createElement("div");
+    text.textContent = `${cloud[i]} %`;
+    text.classList.add("c");
+    resultArea.appendChild(text);
+  }
+};
+
 const showCoffee = (cup) => {
   const resultArea = document.querySelector(".coffee__show");
   const img = document.createElement("img");
-  // img.src = `https://cataas.com${cat}`;
-  console.log(cup);
+  // console.log(cup);
   resultArea.appendChild(img);
 };
 const showCat = (cat) => {
@@ -56,6 +116,6 @@ const showCat = (cat) => {
   resultArea.appendChild(img);
 };
 
-Weather();
-Coffee();
+// WYKONAJ API
+Geo();
 Cats();
