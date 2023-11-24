0f3b02e54d46ac0520bb82f91a8462c3ff06e7db
Artur Matysiak <inz.matys@gmail.com>
2023-11-24 19:31:04 +0100

    refactoring code, remove cat API, add random cat from url

--- main.js
+++ main.js
@@ -2,16 +2,12 @@ let longitudeGeo;
 let latitudeGeo;
 let cityGeo;
 
-// API DOWNLOAD GEOLOCALIZATION (from IP)
-const Geo = () => {
-  const urlGeo = "http://ip-api.com/json/?fields=query,lat,lon,city";
-  fetch(urlGeo)
+// API DOWNLOAD GEOLOCATION (from IP)
+Geo = () => {
+  fetch("http://ip-api.com/json/?fields=query,lat,lon,city")
     .then((geo) => {
-      if (geo.status !== 200) {
-        throw Error("To nie jest odp. 200");
-      } else {
-        return geo.json();
-      }
+      if (geo.status !== 200) throw Error("Hmmm, something is wrong!");
+      else return geo.json();
     })
     .then((geo) => {
       longitudeGeo = geo.lon;
@@ -19,60 +15,30 @@ const Geo = () => {
       cityGeo = geo.city;
       Weather();
     })
-    .catch((err) => alert(err, "coś z Geolokalizacją jest nie tak!"));
+    .catch((err) => alert(err, "Hmmm, something is wrong with Geolocation!"));
 };
 
 // API DOWNLOAD WEATHER
-const Weather = () => {
+Weather = () => {
   const urlWeather = `https://api.open-meteo.com/v1/forecast?latitude=${latitudeGeo}&longitude=${longitudeGeo}&hourly=temperature_2m,cloudcover,windspeed_80m,temperature_180m&models=gfs_global&current_weather=true&forecast_days=1&timezone=Europe%2FBerlin`;
 
-  // Podstawowa! budowa metody fetch()
   fetch(urlWeather)
     .then((climate) => {
-      if (climate.status !== 200) {
-        throw Error("To nie jest odp. 200");
-      } else {
-        return climate.json(); // parsuje tzn. zamienia wyniki z url na obiekt JS
-      }
+      if (climate.status !== 200) throw Error("Hmmm, something is wrong!");
+      else return climate.json(); // parse from url to object
     })
     .then((climate) => showWeather(climate))
-    .catch((err) => alert(err, "coś z Pogodą jest nie tak!"));
-};
-
-// API WITH LONG WEEKENDS (maybe future)
-// const Weekends = () => {
-//   const urlWeekends = "https://date.nager.at/api/v3/PublicHolidays/2023/PL";
-//   fetch(urlweekends)
-//     .then((days) => {
-//       return days.json();
-//     })
-//     .then((days) => showWeekends(days));
-// };
-
-// API POBIERAJĄCE KOTA
-const Cats = () => {
-  const urlCats = "https://cataas.com/cat?json=true";
-  fetch(urlCats)
-    .then((cats) => {
-      if (cats.status !== 200) {
-        throw Error("Nie pobiera kotów!!!");
-      } else {
-        return cats.json();
-      }
-    })
-    // .then((cat) => console.log(cat))
-    .then((cat) => showCat(cat.url))
-    .catch(Error("coś z Kotami jest nie tak!"));
+    .catch((err) => alert(err, "Hmmm, something is wrong with this weather!"));
 };
 
 // SHOW RESULTS in DOM
-const showWeather = (climate) => {
+showWeather = (climate) => {
   const resultArea = document.querySelector(".weather__grid");
 
   // add city + date
   const city = document.createElement("div");
   const temp = [...climate.hourly.temperature_2m];
-  city.textContent = `${cityGeo} (twoja lokalizacja wg ISP), aktualizacja ${climate.current_weather.time}`;
+  city.textContent = `${cityGeo} (Your location), update ${climate.current_weather.time}`;
   city.classList.add("city__my");
   resultArea.appendChild(city);
 
@@ -103,28 +69,36 @@ const showWeather = (climate) => {
   }
 };
 
+showCoffee = () => {
+  const img = document.createElement("img");
+  img.src = `https://coffee.alexflipnote.dev/random`;
+  document.querySelector(".coffee__show").appendChild(img);
+};
+
+showCat = (cat) => {
+  const img = document.createElement("img");
+  img.src = `https://cataas.com/cat`;
+  document.querySelector(".cats__show").appendChild(img);
+};
+
+Geo();
+showCoffee();
+showCat();
+
 // FUTURE
+//
+// API WITH LONG WEEKENDS (maybe future)
+// const Weekends = () => {
+//   const urlWeekends = "https://date.nager.at/api/v3/PublicHolidays/2023/PL";
+//   fetch(urlweekends)
+//     .then((days) => {
+//       return days.json();
+//     })
+//     .then((days) => showWeekends(days));
+// };
+
 // const showWeekends = (days) => {
 //   const resultArea = document.querySelector(".weekends__show");
 //   const weekends = document.createElement("div");
 //   resultArea.appendChild(img);
 // };
-
-const showCoffee = (cup) => {
-  const resultArea = document.querySelector(".coffee__show");
-  const img = document.createElement("img");
-  img.src = `https://coffee.alexflipnote.dev/random`;
-  resultArea.appendChild(img);
-};
-
-const showCat = (cat) => {
-  const resultArea = document.querySelector(".cats__show");
-  const img = document.createElement("img");
-  img.src = `https://cataas.com${cat}`;
-  resultArea.appendChild(img);
-};
-
-// RUN API
-Geo();
-showCoffee();
-Cats();
